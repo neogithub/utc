8b538ff4d40cfa616f207042192af1e39f0b2ad1
neogithub <evan.buxton@neoscape.com>
2015-02-24 09:12:50 -0500

    Legal close 'X' added to title string, signing updated

--- xhSplitView/masterViewController.m
+++ xhSplitView/masterViewController.m
@@ -142,7 +142,7 @@ static CGFloat yHeight = 90;
     [self.view makeToast:@"Agion® is a registered trademark of Sciessent LLC.\n\nBACnet® is a registered trademark of American Society of Heating, Refrigerating and Air-Conditioning Engineers (ASHRAE).\n\nLEED® is a registered trade mark of the U.S. Green Building Council."
                 duration:10.0
                 position:CSToastPositionBottom
-                   title:@"Legal Notices"];
+                   title:@"Legal Notices                       X"];
 }
 
 -(void)initVersion
